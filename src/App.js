a834c5f60a6b442403de000c661a8fa5818022f5
Vish <vishvajitravalji@gmail.com>
2021-07-22 15:11:59 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
@@ -26,25 +25,14 @@ const DUMMY_EXPENSES = [
   },
 ];
 
-
-
 const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
- const [expenses,setExpenses] = useState(DUMMY_EXPENSES);
-
- const addExpenseHandler = expense => {
-
-  setExpenses((prevExpenses) => {
-    return [expense, ...prevExpenses];
-  });
-};
-
-  return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
-    </div>
-  );
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   // return React.createElement(
   //   'div',
@@ -53,6 +41,12 @@ const App = () => {
   //   React.createElement(Expenses, { items: expenses })
   // );
 
-}
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
 
 export default App;
